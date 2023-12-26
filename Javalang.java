ed4debaca866eac1a5c6ecb7a6fc994d56de132f
jaybhuptani10 <115783493+jaybhuptani10@users.noreply.github.com>
2023-12-26 16:21:46 +0530

    Add files via upload

---
+++ Javalang.java
@@ -0,0 +1,16 @@
+public class Javalang {
+    public static void main(String[] args) {
+
+        int intVar = 1;
+        float floatVar = 2.3f;
+        char charVar = 'J';
+        double doubleVar = 2.731458;
+        boolean boolVar = true;
+
+        System.out.println("Integer: " + intVar);
+        System.out.println("Float: " + floatVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
